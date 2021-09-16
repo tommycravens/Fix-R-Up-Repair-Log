844d70790e8ca15ce946a49c06c51fc68c0c56d2
tommycravens <joe.t.cravens92@gmail.com>
2021-09-16 16:51:54 -0400

    Trying to set up the back end equip. model & route

---
+++ app/models/equipment.js
@@ -0,0 +1,34 @@
+const { version } = require('mongoose')
+const mongoose = require('mongoose')
+
+const equipmentSchema = new mongoose.Schema(
+	{
+    equipmentType: {
+      type: String,
+      required: true,
+    },
+		modelName: {
+			type: String,
+			required: true,
+		},
+    modelNumber: {
+      type: String,
+      required: true,
+    },
+		serialNumber: {
+			type: String,
+			required: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+//  Notes: and in service dates or photos to add in files and buttons for receipts, insurance, title or whatever else but will need to be listed as an attribute under equipment
+module.exports = mongoose.model('Equipment', equipmentSchema)
